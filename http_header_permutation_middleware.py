3ba29446e3d0ef9d3693c74db6f4ad0ecaccb3d2
Sergey Semushin <kezzyhko@gmail.com>
2022-03-03 12:41:44 +0300

    Fix covert message allignment issues

--- http_header_permutation_middleware.py
+++ http_header_permutation_middleware.py
@@ -11,14 +11,19 @@ def http_header_permutation_middleware(get_response):
         response = get_response(request)
 
         covert_message_left = request.session.get('covert_message_left', full_covert_message)
-        if covert_message_left == 0:
-            covert_message_left = full_covert_message
+        covert_bits_sent = request.session.get('covert_bits_sent', 0)
 
         sorted_headers_keys = sorted(response.headers.keys(), key = lambda x: x.lower())
         hidden_bits_amount = math.floor(math.log2(math.factorial(len(sorted_headers_keys))))
 
+        bits_left = covert_message_left.bit_length()
+        if bits_left < hidden_bits_amount:
+            covert_message_left += full_covert_message << (bits_left + (8 - (covert_bits_sent + bits_left) % 8))
+
         number_to_send = covert_message_left & ((1 << hidden_bits_amount) - 1)
+
         request.session['covert_message_left'] = covert_message_left >> hidden_bits_amount
+        request.session['covert_bits_sent'] = covert_bits_sent + hidden_bits_amount
 
         new_headers = {}
         while True:
